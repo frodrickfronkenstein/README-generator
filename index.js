bab20b341d9674c602075ca9a5a4102c2377336b
frodrickfronkenstein <ericandrewsmith88@gmail.com>
2022-07-19 20:41:48 -0400

    writefile added

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 // TODO: Include packages needed for this application
 const generateMarkdown = require('./utils/generateMarkdown.js');
 const inquirer = require("inquirer");
+const fs = require('fs');
+const { reject } = require('bcrypt/promises.js');
+const { resolve } = require('path');
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {}
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile('./dist/README.md', err => {
+        if (err) {
+            reject(err);
+            return;
+        }
+        resolve({
+            ok: true,
+            message: 'README File created'
+        });
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    
+}
 
 // Function call to initialize app
 init();
+
 
 console.log('hello node!');
